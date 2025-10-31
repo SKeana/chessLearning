889e6898c81aadeec1060e153174ede242fbc0b4
skeana <skean2000@outlook.com>
2025-10-31 08:50:17 +0000

    import problemes

---
+++ chess_engine.py
@@ -0,0 +1,9 @@
+import chess
+
+def start_game():
+    """
+    Create a new chess board and return the starting position in FEN format.
+    """
+    # TODO: Create a new board
+    # TODO: Return the FEN string
+    pass
