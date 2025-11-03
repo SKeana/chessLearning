2f774b99bc4e01fb95af0b0df7ac3dd66a1443f8
skeana <skean2000@outlook.com>
2025-11-03 13:40:30 +0000

    'working'

--- chess_engine.py
+++ chess_engine.py
@@ -1,9 +1,5 @@
 import chess
 
 def start_game():
-    """
-    Create a new chess board and return the starting position in FEN format.
-    """
-    # TODO: Create a new board
-    # TODO: Return the FEN string
-    pass
+    board = chess.Board()
+    return board.fen()   
